b3b08fa7818c10c55c423000f5fc4c7c0c80ca59
Priti Kumari <pritikumari2559@gmail.com>
2026-06-05 17:34:59 +0000

    conversion and operation done

---
+++ 01_Basics/conversionAndoperations.js
@@ -0,0 +1,47 @@
+let score="123ab"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+/*"33"=>33
+"33abc"=>NaN
+true=>1
+false=>0
+null=>0
+undefined=>NaN
+*/
+
+// let isloggedIn=1
+// let booleanisloggedIn=Boolean(isloggedIn)
+// console.log(booleanisloggedIn)
+
+//1=>true
+//0=>false
+//null=>false
+//undefined=>false
+//NaN=>false
+//" "=>true
+//"Priti"=>true
+
+
+/****************Operations****************/
+let value=3;
+let negvalue=-value;
+// console.log(negvalue)
+
+let str1="priti"
+let str2="kumari"
+let str3=str1+str2
+// console.log(str3)
+
+console.log("1"+2) //12
+console.log(1+"2") //12
+console.log("1"+2+3) //123
+console.log(1+2+"3") //33
+
+console.log(+true) //1
+console.log(+"") //0
+console.log(+"Priti") //NaN
