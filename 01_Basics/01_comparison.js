ba2992a15cc0ce201b6b8d6ce8091e7af1388be1
Priti Kumari <pritikumari2559@gmail.com>
2026-06-05 18:01:13 +0000

    datatypes_summary

---
+++ 01_Basics/01_comparison.js
@@ -0,0 +1,23 @@
+console.log("2>1") //true
+console.log("2<1") //false
+console.log("2>=1") //true
+console.log("2<=1") //false
+console.log("2==1") //false
+console.log("2!=1") //true
+
+//strict equality operator
+console.log("2"==2) //true
+console.log("2"===2) //false
+console.log("2"!=2) //false
+console.log("2"!==2) //true
+
+//null and undefined
+console.log(null==undefined) //true
+console.log(null===undefined) //false
+
+console.log(null==0) //false
+console.log(undefined==0) //false
+console.log(null>=0) //true
+console.log(undefined>=0) //false
+console.log(null<=0) //true
+console.log(undefined<=0) //false
---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,25 @@
+//primitive---> String,Number,Boolean,Null,Undefined ,Symbol,BigInt
+
+
+
+const score=100
+const scoreValue=122.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+const id=Symbol("id")
+const bigNumber=1234567890123456789012345678901234567890n
+
+//Non-primitive--->Object,Array,Function
+
+//Array
+const heros=["Shaktiman","Doga","Batman"]
+const myObj={
+    name:"Priti",
+    age:22,
+    isLoggedIn:false
+}
+
+let myFunction=function(){
+    console.log("Hello World")
+}
