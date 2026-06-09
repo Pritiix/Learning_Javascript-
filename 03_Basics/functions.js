35b0b8304bcbe41d481ccae39d9c0b793f3def66
Priti Kumari <pritikumari2559@gmail.com>
2026-06-09 15:30:30 +0000

    functions done

---
+++ 03_Basics/functions.js
@@ -0,0 +1,22 @@
+// function myfxn(){
+//     console.log("A")
+// }
+
+// myfxn()
+
+// function addnumbers(num1,num2){
+//     console.log(num1+num2)
+// }
+
+function addnumbers(num1,num2){
+    // console.log(num1+num2)
+}
+addnumbers(3,4)
+
+function sum(n1,n2){
+    let result=n1+n2;
+    return result;
+}
+const result=sum(3,4)
+console.log("Result is:",result)
+
