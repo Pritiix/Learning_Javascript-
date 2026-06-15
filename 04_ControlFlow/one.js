432932491dab0a0d284d81cc0cbb2dda41626a1f
Priti Kumari <pritikumari2559@gmail.com>
2026-06-15 13:45:37 +0000

    controlFlow done

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,33 @@
+// const a=34;
+// const b=23;
+
+// if(a>b){
+//    console.log("a is greater than b")
+// }
+// else{
+//     console.log("b is greater than a")
+// }
+// score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`You are super hero with power of ${power}`)
+// }
+
+// console.log(power) //not executed as power is in block scope
+
+// //nesting
+// const age=18
+// if(age>18){
+//     console.log("You are eligible for voting")
+//     const weight=50
+//     if(weight>40){
+//         console.log("You are eligible for army")
+//     }
+//     else{
+//         console.log("You are not eligible for army")
+//     }
+// }
+// else{
+//     console.log("You are not eligible for voting")
+// }
+
---
+++ 04_ControlFlow/truthy.js
@@ -0,0 +1,7 @@
+//falsy value
+
+//false,0,-0,BigInt 0n ,null,undefined,NaN,""
+
+//truthy value
+
+//" ",[],{},function(){},new Date(),-1,Infinity,-Infinity,BigInt 1n,'0' "false"
